6bd4f68efa978ca71f3f4e4558f3f00e94d372aa
Majd <majd4.sh@gmail.com>
2024-12-17 15:45:06 +0400

    add edit button

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import "react-loading-skeleton/dist/skeleton.css";
 import Skeleton from "react-loading-skeleton";
 import { Box } from "@radix-ui/themes";
-import delay from "delay";
 
 const LoadingNewIssuePage = async () => {
-  await delay(2000);
   return (
     <Box>
-      <Skeleton />
-      <Skeleton height={"20rem"} />
+      <Skeleton className={"max-w-lg"} />
+      <Skeleton className={"max-w-xl"} height={"20rem"} />
     </Box>
   );
 };
