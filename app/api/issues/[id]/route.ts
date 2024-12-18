eaa9ca1ef13d39073e0f48e9829b3cecd8125326
Majd <majd4.sh@gmail.com>
2024-12-18 14:36:15 +0400

    assign issues api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { AuthOptions, getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -8,14 +8,26 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
-  const session = await getServerSession(authOptions as AuthOptions);
-  if (!session) {
-    return NextResponse.json({}, { status: 401 });
-  }
+  // const session = await getServerSession(authOptions as AuthOptions);
+  // if (!session) {
+  //   return NextResponse.json({}, { status: 401 });
+  // }
   const body = await request.json();
   const id = (await params).id;
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success) return NextResponse.json(validation.error.format());
+
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: body.assignedToUserId,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid user" }, { status: 400 });
+    }
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
@@ -33,6 +45,7 @@ export async function PATCH(
     data: {
       title: body.title,
       description: body.description,
+      assignedToUserId: body.assignedToUserId,
     },
   });
 
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,5 +2,11 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
   title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  description: z.string().max(65435).min(1),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1).max(255).optional(),
+  description: z.string().min(1).max(65435).optional(),
+  assignedToUserId: z.string().min(1).max(255).optional().nullable(),
 });
