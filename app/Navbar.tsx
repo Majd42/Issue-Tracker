a02b7e9d74270cf4ed07a73819b19138c22a2ab8
Majd <majd4.sh@gmail.com>
2024-12-18 12:49:39 +0400

    add loading skeleton

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -13,6 +13,8 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Navbar = () => {
   return (
@@ -61,7 +63,7 @@ const NavLinks = () => {
 
 const AuthStatus = () => {
   const { status, data: session } = useSession();
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width={"3rem"} />;
   if (status === "unauthenticated")
     return (
       <Link className="nav-link" href={"/api/auth/signin"}>
