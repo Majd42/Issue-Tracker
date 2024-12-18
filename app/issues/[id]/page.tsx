9af299cce6717c8723448e492c5d78f045673b0e
Majd <majd4.sh@gmail.com>
2024-12-18 10:02:57 +0400

    add delete button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 type Props = {
   params: Promise<{
@@ -22,13 +23,14 @@ const IssueDetailPage = async ({ params }: Props) => {
     notFound();
   }
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
-      <Box>
+    <Grid columns={{ initial: "1", md: "5" }} gap={"5"}>
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
+      <Flex direction={"column"} gap={"4"}>
         <EditIssueButton id={id} />
-      </Box>
+        <DeleteIssueButton issueId={id} />
+      </Flex>
     </Grid>
   );
 };
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import "react-loading-skeleton/dist/skeleton.css";
-import Skeleton from "react-loading-skeleton";
-import { Box } from "@radix-ui/themes";
 import LoadingIssueForm from "../LoadingIssueForm";
 
 const LoadingNewIssuePage = async () => {
