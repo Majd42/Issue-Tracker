fbccd92943773be20f0871352281e1cd5282b497
Majd <majd4.sh@gmail.com>
2024-12-19 09:02:31 +0400

    adding issue summary and latest issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div className=""></div>;
+import prisma from "@/prisma/client";
+import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: {
+      status: "OPEN",
+    },
+  });
+  const closed = await prisma.issue.count({
+    where: {
+      status: "CLOSED",
+    },
+  });
+  const inProgress = await prisma.issue.count({
+    where: {
+      status: "IN_PROGRESS",
+    },
+  });
+  return (
+    <>
+      <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+      <LatestIssues />
+    </>
+  );
 }
